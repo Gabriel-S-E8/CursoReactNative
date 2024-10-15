55be64ee887164661dac401808058b82ea19f199
Gabriel.Trabalho <gabrielnup@outlook.com>
2024-10-15 17:52:21 -0300

    Começo do primeiro projeto com react-native puro

---
+++ exercicios/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+  printWidth: 80,
+  tabWidth: 4,
+  singleQuote: true,
+  trailingComma: 'all',
+};
---
+++ exercicios/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text} from 'react-native';
+import Primeiro from './components/primeiro';
+
+const App = () => {
+    return (
+        <Text>
+            Hello World
+            <Primeiro/>    
+        </Text>
+        
+
+    )
+}
+
+export default App;
